7b6cef365c1cbf5dc62c73920223d35d0c5c1a6a
noxiii <noxiii@nippox.de>
2024-03-07 00:15:45 +0100

    add mkdir

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+
+mkdir -p download
 curl 'https://www.wfs.nrw.de/geobasis/wfs_nw_alkis_aaa-modell-basiert?service=WFS&version=2.0.0&request=getFeature&TYPENAMES=adv:AX_Gemeinde' > download/gemeinde.xml
 
 curl 'https://www.wfs.nrw.de/geobasis/wfs_nw_alkis_aaa-modell-basiert?service=WFS&version=2.0.0&request=getFeature&TYPENAMES=adv:AX_LagebezeichnungKatalogeintrag' > download/strassen.xml
