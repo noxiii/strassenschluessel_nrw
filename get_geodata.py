892998840e4a31a4f603a1661648bcdcb40c6887
noxiii <noxiii@nippox.de>
2024-03-27 09:48:29 +0100

    disable full list of gpd

--- get_geodata.py
+++ get_geodata.py
@@ -19,7 +19,7 @@ import os
 from requests import Request
 from urllib.parse import unquote
 
-pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_columns', None)
 
 
 class streets_of_nrw:
